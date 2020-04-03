5289871f1e141f83e203b72059cbb11ef680be63
gthankgod <gthankgod@gmail.com>
2020-04-03 12:58:13 +0100

    commit

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -4,7 +4,17 @@ import { ExamContext } from '../../context/ExamContextProvider'
 
 const FormField = () => {
     let { exam } = useContext(ExamContext);
-    let [option, setOption ] = useState([]);
+    let [optValue, setOptValue ] = useState('');
+    let [options, setOptions ] = useState([]);
+
+
+    const onClickOpt = (e) => {
+      setOptValue(e.target.value)
+    };
+
+    const onClickOptAdd = () => {
+      setOptions([...options,optValue ])
+    }
     return (
         <Form className="mt-4">
        <ProgressBar animated now={exam.numberofquestions} />
@@ -12,27 +22,28 @@ const FormField = () => {
           <Form.Row>
             <Form.Group as={Col} controlId="formGridEmail">
               <Form.Label>Question</Form.Label>
-              <Form.Control type="text" placeholder="Enter email" />
+              <Form.Control type="text" placeholder="Add Question" />
             </Form.Group>
           </Form.Row>
 
           <Form.Row>
             <Form.Group as={Col} controlId="formGridEmail">
               <Form.Label>Add Options</Form.Label>
-              <Form.Control type="text" placeholder="Enter email" />
+              <Form.Control type="text" placeholder="Add option" onClick={e => onClickOpt(e)}/>
             </Form.Group>
-            < Button variant="primary" type="button"> Add Options </Button>
+            < Button variant="primary" type="button" as={Col} onClick={onClickOptAdd}> Add </Button>
           </Form.Row>
-
+          <ul>
+          { options.map(option => {
+                return <li>{option}</li>
+             })
+          }</ul>
           <Form.File 
             id="custom-file"
             label="Add an image"
             custom
           />
 
-          <Form.Group id="formGridCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
 
           <Button variant="primary btn-block" type="submit">
             Submit
