7bcb062e64f29a0b4fe6bb7cd4bd2a53690ef7ee
gthankgod <gthankgod@gmail.com>
2020-04-05 01:32:16 +0100

    commit

---
+++ client/src/components/Success.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Jumbotron } from 'react-bootstrap'
+
+const SuccessPage = (props) => {
+    return (
+        <Jumbotron className="mt-4">
+            <h1>Exam Succesfully Uploaded</h1>
+            <p>
+                For more info on how to add more features to this question bank, react out to admin@urimmng.com
+            </p>
+            <p>
+                <Link variant="primary" to="/">Add New Exam</Link>
+            </p>
+        </Jumbotron>
+    )
+}
+
+export default SuccessPage
