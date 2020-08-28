9d1ae31a1007e43922454bb47ad10939eb99e5d0
gthankgod <gthankgod@gmail.com>
2020-08-28 18:19:15 +0100

    new commit 28-08-2020

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -36,7 +36,6 @@ const FormField = (props) => {
                               });
               
             const imgData = await imgRes.json();
-            console.log(imgData);
             setQuestion({...Question, image: imgData.data })
 
             } catch (error) {
